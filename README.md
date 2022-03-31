fac0a857bdb882d9066a9cc7f15b03254904bf48
SheensonSV <ssheenson85@gmail.com>
2022-03-31 18:16:30 +0400

    Added circuit breaker for not running on non-MacOsX Systems. Added exit on right mouse click on ball at tray.

--- README.md
+++ README.md
@@ -24,12 +24,7 @@ Just run
 
 ## Built With
 
-* [BrainInHead](http://findthemsomewere/) - and did the same :^)
-
-## Contributing
-
-Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
-
+* [BrainInHead](http://itisinheads/) :^)
 
 ## Authors
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,27 +2,31 @@ package main;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    private static Runtime runtime = Runtime.getRuntime();
-    private static String[] commands = {"sysctl", "-a"}; // | grep machdep.xcpm.cpu_thermal_level:"};
+    private static final Runtime runtime = Runtime.getRuntime();
+    private static final String[] commands = {"sysctl", "-a"}; // | grep machdep.xcpm.cpu_thermal_level:"};
     private static Process process = null;
     private static String s = null;
-    private static String findingLine = "machdep.xcpm.cpu_thermal_level: ";
+    private static final String findingLine = "cpu_thermal_level: ";
     private static Integer temperatureCPU = 0;
 
     public static void main(String[] args) {
+        if (!System.getProperties().getProperty("os.name").equals("Mac OS X")) {
+            System.exit(0);
+        }
         int lastTemperature = 0;
-        int currentTemperature = 0;
+        int currentTemperature;
         SystemTray systemTray = SystemTray.getSystemTray();
         TrayIcon trayIcon = null;
         try {
             while (true) {
-
                 currentTemperature = getTemperature();
                 if (currentTemperature < 1) currentTemperature = 1;
                 if (currentTemperature > 99) currentTemperature = 99;
@@ -34,6 +38,22 @@ public class Main {
 
                     Image icon = ImageIO.read(new File(source));
                     trayIcon = new TrayIcon(icon, "CPUTemp");
+                    trayIcon.addMouseListener(new MouseListener() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            if (e.getButton() == 3) {
+                                System.exit(0);
+                            }
+                        }
+                        @Override
+                        public void mousePressed(MouseEvent e) {}
+                        @Override
+                        public void mouseReleased(MouseEvent e) {}
+                        @Override
+                        public void mouseEntered(MouseEvent e) {}
+                        @Override
+                        public void mouseExited(MouseEvent e) {}
+                    });
                     systemTray.add(trayIcon);
 
                     lastTemperature = currentTemperature;
@@ -41,9 +61,7 @@ public class Main {
                 else {
                     Thread.sleep(2000);
                 }
-
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -60,12 +78,13 @@ public class Main {
             while ((s = stdInput.readLine()) != null) {
 
                 if (s.contains(findingLine)) {
-                    temperatureCPU = Integer.parseInt(s.substring(findingLine.length()));
+                    String[] split = Main.s.split(" ");
+                    temperatureCPU = Integer.parseInt(split[1]);
                 }
             }
             //lets be there. may be it helps.
-            while ((s = stdError.readLine()) != null) {
-            }
+//            while ((s = stdError.readLine()) != null) {
+//            }
             stdInput.close();
             stdError.close();
             return temperatureCPU;
