00a50b3baf689b35aae737c8538039b78bc44794
SheensonSV <ssheenson85@gmail.com>
2022-04-01 11:10:16 +0400

    new branch powermetrics contains code whoch used macosx poermetrix util with sudo permission.

--- README.md
+++ README.md
@@ -6,14 +6,21 @@ Shows CPU Temperature in Celsius
 
 This is a program that show a CPU temperature being as boll with number at tray.
 Now it works only on MAC OS X.
+Branch powermetrics contains changed code to expluatate mac os x integrated util powermetrics.
+It works only with sudo credentials. 
+To run this code you need to add environment variable SUDOPSWD=yoursudopassword in run configuration.
+If all is good you will see the ball with numbers at tray.
 To exit - just click right button of mouse on tray's icon.
-It gets a param from systemctl as Integer. Then it goes to find png image file from resources and pull it into the tray.
-Very simple realisation.
+
+Branch master contains code which gets a param from systemctl. In that branch you don't need sudo permission.
+But temperature data is not so precise.
+
 The minimum showing temperature is 1 grad Celsius and the max is 99.
 
 ### Prerequisites
 
-You don't need to install any another tools. But if you are java developer it seems to be that you use Intellij Idea or Eclipse. 
+You don't need to install any another tools. 
+Use Intellij Idea or Eclipse or javac. 
 
 ## Running the tests
 
@@ -33,7 +40,7 @@ Just run
 
 ## License
 
-This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) void file for details
 
 ## Acknowledgments
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,25 +4,24 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-    private static final Runtime runtime = Runtime.getRuntime();
-    private static final String[] commands = {"sysctl", "-a"}; // | grep machdep.xcpm.cpu_thermal_level:"};
+    private static Runtime runtime = Runtime.getRuntime();
+    private static final String commands = "sudo -S powermetrics -s smc -n 1 -i 50 | grep 'CPU die'";
     private static Process process = null;
     private static String s = null;
-    private static final String findingLine = "cpu_thermal_level: ";
+    private static final String findingLine = "CPU die temperature: ";
     private static Integer temperatureCPU = 0;
+    private static final byte[] password = System.getenv("SUDOPSWD").concat("\n\r").getBytes(StandardCharsets.UTF_8);
 
     public static void main(String[] args) {
         if (!System.getProperties().getProperty("os.name").equals("Mac OS X")) {
             System.exit(0);
         }
         int lastTemperature = 0;
-        int currentTemperature;
+        int currentTemperature = 0;
         SystemTray systemTray = SystemTray.getSystemTray();
         TrayIcon trayIcon = null;
         try {
@@ -69,24 +68,34 @@ public class Main {
 
     public static Integer getTemperature() {
         BufferedReader stdInput = null;
-        BufferedReader stdError = null;
+//        BufferedReader stdError = null;
+        OutputStream outputStream = null;
         try {
             process = runtime.exec(commands);
             stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            //need to use sudo permission
+            outputStream = process.getOutputStream();
+            outputStream.write(password);
+            outputStream.flush();
 
             while ((s = stdInput.readLine()) != null) {
-
                 if (s.contains(findingLine)) {
                     String[] split = Main.s.split(" ");
-                    temperatureCPU = Integer.parseInt(split[1]);
+                    String temperatureTextFromArray = split[3];
+                    temperatureCPU = (int) Double.parseDouble(temperatureTextFromArray);
                 }
             }
+
             //lets be there. may be it helps.
+//            stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 //            while ((s = stdError.readLine()) != null) {
+//                System.out.println(s);
 //            }
+
             stdInput.close();
-            stdError.close();
+//            stdError.close();
+            outputStream.close();
             return temperatureCPU;
         } catch (IOException e) {
             e.printStackTrace();
@@ -94,7 +103,8 @@ public class Main {
         finally {
             try {
                 stdInput.close();
-                stdError.close();
+//                stdError.close();
+                outputStream.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
