20ce3e805633b29d1519768b9a9e95b2331c324d
SheensonSV <ssheenson85@gmail.com>
2022-04-04 09:19:44 +0400

    Changed imports in head of main

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,7 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.*;
-import java.util.Arrays;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class Main {
     private static Runtime runtime = Runtime.getRuntime();
